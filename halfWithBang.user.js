83e560346f36bac9fd1b6c72bfbf598ecdb5f222
fengkx <liangkx8237@gmail.com>
2019-01-09 22:03:04 +0800

    Replace Full width bang to half width in ddg
    Replace Full width bang to half width in ddg

---
+++ halfWithBang.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name 　　　Replace Full width bang to half width
+// @name:zh-CN  　替换全角！到半角!
+// @namespace https://github.com/fengkx/
+// @match     https://duckduckgo.com/*
+// @match     https://start.duckduckgo.com/*
+// @version 0.1
+// @description       Replace Full width bang！ to half width!
+// @description:en    Replace Full width bang！ to half width!
+// @description:zh    替换全角bang！到半角bang!
+// @description:zh-CN 替换全角bang！到半角bang!
+// @grant none
+//@run-at             document-end
+// ==/UserScript==
+
+var $input = document.getElementById('search_form_input_homepage') || document.getElementById('search_form_input') ;
+$input.addEventListener('input', function (e) {
+	this.value = this.value.replace(/^！/, '!');
+})
